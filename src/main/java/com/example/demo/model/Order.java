0ac251fcd255947984040849ed078c82d72ecb45
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-01-01 13:53:00 +0700

    test done controller order

--- src/main/java/com/example/demo/model/Order.java
+++ src/main/java/com/example/demo/model/Order.java
@@ -7,26 +7,18 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-    @ManyToOne
-    @JoinColumn(name = "home_id")
-    private Home home;
     private Date bookingDate;
     private Date startDate;
     private Date endDate;
 
-    public Order(Long id, User user, Home home, Date bookingDate, Date startDate, Date endDate) {
+    public Order() {
+    }
+
+    public Order(Long id, Date bookingDate, Date startDate, Date endDate) {
         this.id = id;
-        this.user = user;
-        this.home = home;
         this.bookingDate = bookingDate;
         this.startDate = startDate;
         this.endDate = endDate;
-    }
-
-    public Order() {
     }
 
     public Long getId() {
@@ -35,22 +27,6 @@ public class Order {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Home getHome() {
-        return home;
-    }
-
-    public void setHome(Home home) {
-        this.home = home;
     }
 
     public Date getBookingDate() {
