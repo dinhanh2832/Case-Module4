17eb063b41e3cfb68bdb72331cb2d3461aeaabc8
DuyLeVu <levuduy132@gmail.com>
2022-01-04 23:59:31 +0700

    fix bug search by address and status and bedroom

--- src/main/resources/templates/sheltek/script/search.js
+++ src/main/resources/templates/sheltek/script/search.js
@@ -1,30 +1,41 @@
 function searchHome() {
-    // let address = $("location").val();
-    // let content = $("#content").val();
-    // let status = $("#status").val();
     let address = document.getElementById("location").value;
     let bedroom = document.getElementById("bedroom").value;
     // let showerRoom = document.getElementById("bedroom").value;
     // let date = document.getElementById("bedroom").value;
     // let price = document.getElementById("bedroom").value;
-    let searchObj = {
-        address: address,
-        bedroom: bedroom
-        // showerRoom: showerRoom,
-        // date: date,
-        // price: price,
-    }
-    console.log(searchObj);
+    // let searchObj = {
+    //     address: address,
+    //     bedroom: bedroom
+    //     // showerRoom: showerRoom,
+    //     // date: date,
+    //     // price: price,
+    // }
+    // console.log(searchObj);
     $.ajax({
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        type: "POST",
-        url: "http://localhost:8080/api/homes/search/address",
-        data: JSON.stringify(searchObj),
+        type: "GET",
+        url: "http://localhost:8080/api/homes",
         success: function (data) {
-            //truyền vào hàm gì đó ví dụ loadData() ở HTML
+            console.log(data);
+            let arr = [];
+            if (address != "") {
+                for (let i=0; i<data.length; i++){
+                    if (data[i].address == address) arr.push(data[i]);
+                }
+            } else {
+                arr = data;
+            }
+            data = arr;
+            arr = [];
+            if (bedroom != "") {
+                for (let i=0; i<data.length; i++){
+                    if (data[i].bedroom == bedroom) arr.push(data[i]);
+                }
+            } else {
+                arr = data;
+            }
+            data = arr;
+            arr = [];
             console.log(data);
         }
     })
