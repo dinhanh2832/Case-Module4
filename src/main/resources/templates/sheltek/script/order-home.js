9a1a6ae4ff93498c435ba2fcf6c464f283fe91c3
DuyLeVu <levuduy132@gmail.com>
2022-01-08 00:39:46 +0700

    done order js

--- src/main/resources/templates/sheltek/script/order-home.js
+++ src/main/resources/templates/sheltek/script/order-home.js
@@ -1,32 +1,34 @@
-function orderHome(){
+function orderHome() {
     // let idH = localStorage.getItem("idHome");
     let idH = 2;
 
-    // let strStartDate = document.getElementById("monthStart").value;
-    // let strEndDate = document.getElementById("monthEnd").value;
+    let strStartDate = document.getElementById("monthStart").value;
+    let strEndDate = document.getElementById("monthEnd").value;
     let startDate = new Date(document.getElementById("monthStart").value);
     let endDate = new Date(document.getElementById("monthEnd").value);
     let oneDay = 86400000;
 
-    console.log(startDate);
-    console.log(endDate);
+    // console.log(startDate);
+    // console.log(endDate);
     if (endDate >= startDate) console.log("true");
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/homeTimes/searchByHome/" + idH,
         success: function (data) {
-            for (let i = )
-                for (let i = startDate.getTime(); i <= endDate.getTime(); i += oneDay) {
-                    console.log(i);
-                    let date1 = new Date(i);
-                    // homeTimeService.save(new HomeTime(date1, order.getHome(), "1"));
-                    console.log(date1);
+            let flag;
+            for (let i = startDate.getTime(); i <= endDate.getTime(); i += oneDay) {
+                let orderDate = new Date(i);
+                console.log(orderDate);
+                for (let j = 0; j < data.length; j++) {
+                    let bookingDate = (new Date(data[j].date));
+                    console.log(bookingDate)
+                    if ((orderDate.getDay() === bookingDate.getDay()) && (orderDate.getMonth() === bookingDate.getMonth()) && (orderDate.getFullYear() === bookingDate.getFullYear())) {
+                        flag = false;
+                        break;
+                    }
                 }
-            for (let i = 0; i < data.length; i++) {
-
-                console.log(new Date(data[i].date));
-                // if (data[i].bedroom == bedroom) arr.push(data[i]);
             }
+            if (!flag) alert("Nhà đang được cho thuê");
         }
-})
+    })
 }
