4f07865144ff1a7f8449743b39706bd4c050070f
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-01-10 00:44:11 +0700

    edit html

--- src/main/resources/templates/sheltek/script/forRent.js
+++ src/main/resources/templates/sheltek/script/forRent.js
@@ -6,49 +6,54 @@ function houseForRent() {
                                     <div class="login-account p-30 box-shadow">
                                         <div class="row">
                                         <div class="col-sm-3"></div>
-                                        <div class="col-sm-6">
-                                        <h5 class="mb-50 text-center">CHO THUÊ</h5>
+                                        <div class="col-sm-6"><br>
+                                        <h5 class="mb-50 text-center f1">CHO THUÊ</h5>
                                          <div class="col-sm-6">
-                                                <input type="text"  placeholder="Tên nhà" id="username">
+                                                <input class="f1" type="text" style="font-family: 'Comforter', cursive;
+                                                font-family: 'Open Sans', sans-serif;"  placeholder="Tên nhà" id="username">
                                             </div>
                                             <div class="col-sm-6">
-                                                <input type="text"  placeholder="Địa chỉ" id="address">
+                                                <input class="f1" style="font-family: 'Comforter', cursive;
+                                                font-family: 'Open Sans', sans-serif;" type="text"  placeholder="Địa chỉ" id="address">
                                             </div>
-                                            <div class="col-sm-6" id="category">                                                                                                                                                                                                   
+                                            <div class="col-sm-6 f1"  id="category">                                                                                                                                                                                                   
                                             </div>                                    
                                             <div class="col-sm-6">
-                                            <input type="text" placeholder="Số phòng ngủ" id="bedroom">                                                                                                                        
+                                            <input class="f1" type="text" style="font-family: 'Comforter', cursive;
+                                                font-family: 'Open Sans', sans-serif;" placeholder="Số phòng ngủ" id="bedroom">                                                                                                                        
                                             </div>                                       
                                             <div class="col-sm-6">
-                                            <input type="text" placeholder="Số phòng tắm" id="showerRoom"> 
+                                            <input class="f1" type="text" style="font-family: 'Comforter', cursive;
+                                                font-family: 'Open Sans', sans-serif;" placeholder="Số phòng tắm" id="showerRoom"> 
                                             </div>                                       
                                             <div class="col-sm-6">
-                                                <input type="text"  placeholder="Giá cho thuê" id="price">
+                                                <input class="f1" type="text" style="font-family: 'Comforter', cursive;
+                                                font-family: 'Open Sans', sans-serif;"  placeholder="Giá cho thuê" id="price">
                                             </div>
-                                            <input type="text"  placeholder="Mô tả" id="description"> 
+                                            <input class="f1" type="text"  placeholder="Mô tả" id="description"> 
                                             <form enctype="multipart/form-data" id="form">
                                             <div class="col-sm-12">
-                                            <p>Ảnh đại diện</p>
+                                            <p class="f1 f4">Ảnh đại diện</p>
                                             <input type="file" name="files"/>
                                             </div>
                                             <div class="col-sm-12">
-                                            <p>Phòng ngủ</p>
+                                            <p class="f1 f4">Phòng ngủ</p>
                                             <input type="file" name="files"/>
                                             </div> 
                                             <div class="col-sm-12">
-                                            <p>Phòng khách</p>
+                                            <p class="f1 f4">Phòng khách</p>
                                             <input type="file" name="files"/>
                                             </div>                                           
                                             <div class="col-sm-12">
-                                            <p>Phòng bếp</p>
+                                            <p class="f1 f4">Phòng bếp</p>
                                             <input type="file" name="files"/>
                                             </div>
                                             <div class="col-sm-12">
-                                            <p>Phòng tắm</p>
+                                            <p class="f1 f4">Phòng tắm</p>
                                             <input type="file" name="files"/>
                                             </div>                                            
-                                            <button class="submit-btn-1 mt-20" type="submit" onclick="createHome()" >Đăng Cho Thuê</button>                                                                                 
-                                            <button class="submit-btn-1 mt-20" type="submit"  >Quay Lại</button>                                                                                 
+                                            <button class="submit-btn-1 mt-20 f1" type="submit" onclick="createHome()" >Đăng Cho Thuê</button>                                                                                 
+                                            <button class="submit-btn-1 mt-20 f1" type="submit"  >Quay Lại</button>                                                                                 
                                             </form>                                        
                                          </div>
                                       <div class="col-sm-3"></div>                                  
@@ -57,13 +62,13 @@ function houseForRent() {
                                 </div>
                             </div>`;
     html +=`<!-- OUR AGENTS AREA START -->
-                    <div class="our-agents-area pt-115 pb-55">
+                  <div class="our-agents-area pt-115 pb-55">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="section-title-2 text-center">
-                                        <h2>Các đại lý của chúng tôi</h2>
-                                        <p>Sheltek có một hệ thống đại lý phong phú và phủ sóng khắp cả nước</p>
+                                        <h2 class="f1">Các đại lý của chúng tôi</h2>
+                                        <p class="f1 f4">Sheltek có một hệ thống đại lý phong phú và phủ sóng khắp cả nước</p>
                                     </div>
                                 </div>
                             </div>
@@ -74,22 +79,19 @@ function houseForRent() {
                                         <div class="col-md-4 col-sm-6 col-xs-12">
                                             <div class="single-agent">
                                                 <div class="agent-image">
-                                                    <img src="images/agents/2.jpg" alt="">
+                                                     <img src="/templates/sheltek/images/admin/1.jpg" alt="">
                                                 </div>
                                                 <div class="agent-info">
                                                     <div class="agent-name">
-                                                        <h5><a href="agent-details.html">Nguyễn Quốc Khánh</a></h5>
-                                                        <p>Đà Nẵng</p>
+                                                        <h5 class="f1 f5"><a href="agent-details.html">Nguyễn Quốc Khánh</a></h5>
+                                                        <p class="f1">Đà nẵng</p>
                                                     </div>
                                                 </div>
                                                 <div class="agent-info-hover">
-                                                    <div class="agent-name">
-                                                        <h5><a href="agent-details.html">Quốc Khánh</a></h5>
-                                                        <p>Đại lý</p>
-                                                    </div>
+                                                
                                                     <ul class="agent-address">
-                                                        <li><img src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
-                                                        <li><img src="images/icons/mail-close.png" alt="">khanh@gmail.com </li>
+                                                        <li><img class="f1" src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
+                                                        <li><img class="f1" src="images/icons/mail-close.png" alt="">khanh@gmail.com </li>
                                                     </ul>
                                                     <ul class="social-media">
                                                         <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
@@ -104,21 +106,18 @@ function houseForRent() {
                                         <div class="col-md-4 col-sm-6 col-xs-12">
                                             <div class="single-agent">
                                                 <div class="agent-image">
-                                                    <img src="images/agents/3.jpg" alt="">
+                                                     <img src="/templates/sheltek/images/admin/2.jpg" alt="">
                                                 </div>
                                                 <div class="agent-info">
-                                                    <div class="agent-name">Nguyễn Đình Ánh</a></h5>
-                                                        <p>Đại lý</p>
+                                                    <div class="agent-name"><h5 class="f1 f5"><a>Nguyễn Đình Ánh</a></h5>
+                                                        <p class="f1">Hà Nội</p>
                                                     </div>
                                                 </div>
                                                 <div class="agent-info-hover">
-                                                    <div class="agent-name">
-                                                        <h5><a href="agent-details.html">Đình Ánh</a></h5>
-                                                        <p>Đại lý</p>
-                                                    </div>
+                                                
                                                     <ul class="agent-address">
-                                                        <li><img src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
-                                                        <li><img src="images/icons/mail-close.png" alt="">anh@gmail.com </li>
+                                                        <li><img class="f1" src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
+                                                        <li><img class="f1" src="images/icons/mail-close.png" alt="">anh@gmail.com </li>
                                                     </ul>
                                                     <ul class="social-media">
                                                         <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
@@ -133,22 +132,19 @@ function houseForRent() {
                                         <div class="col-md-4 col-sm-6 col-xs-12">
                                             <div class="single-agent">
                                                 <div class="agent-image">
-                                                    <img src="images/agents/4.jpg" alt="">
+                                                    <img src="/templates/sheltek/images/admin/3.jpg" alt="">
                                                 </div>
                                                 <div class="agent-info">
                                                     <div class="agent-name">
-                                                        <h5><a href="agent-details.html">Lê Vũ Duy</a></h5>
-                                                        <p>Đại lý</p>
+                                                        <h5 class="f1 f5"><a href="agent-details.html">Lê Vũ Duy</a></h5>
+                                                        <p class="f1">Thành Phố HCM</p>
                                                     </div>
                                                 </div>
                                                 <div class="agent-info-hover">
-                                                    <div class="agent-name">
-                                                        <h5><a href="agent-details.html">Vũ Duy</a></h5>
-                                                        <p>Đại lý</p>
-                                                    </div>
+                                               
                                                     <ul class="agent-address">
-                                                        <li><img src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
-                                                        <li><img src="images/icons/mail-close.png" alt="">duy@gmail.com </li>
+                                                        <li><img class="f1" src="images/icons/phone-2.png" alt="">+1245  785  659 </li>
+                                                        <li><img class="f1" src="images/icons/mail-close.png" alt="">duy@gmail.com </li>
                                                     </ul>
                                                     <ul class="social-media">
                                                         <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
@@ -281,7 +277,7 @@ function showCategory(){
         headers: {"Authorization": 'Bearer ' + localStorage.getItem("token")},
         success: function (data) {
             let html = ``;
-            html +=`<select class="custom-select-2">`;
+            html +=`<select class="custom-select-2 f1">`;
             html += `<option value="0">Loại nhà</option>`;
             for(let i = 0;i< data.length;i++){
                 html +=`<option value="${data[i].id}" id="cate">${data[i].name}</option>`;
