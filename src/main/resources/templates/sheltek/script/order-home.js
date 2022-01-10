28b98eb755570658f4913611f60b920b558f0c38
DuyLeVu <levuduy132@gmail.com>
2022-01-10 19:16:48 +0700

    fix bug tạo đơn thuê nhà, done

--- src/main/resources/templates/sheltek/script/order-home.js
+++ src/main/resources/templates/sheltek/script/order-home.js
@@ -10,9 +10,9 @@ function orderHome() {
     let now = new Date();
     console.log(now);
     // Check ngày tháng nhập vào
-    if ((endDate < now) || (startDate < now)) alert("Bạn đã chọn ngày nhỏ hơn ngày hiện tại, Xin mời nhập lại !")
-    else if (startDate >= endDate) alert("Bạn đã chọn ngày nhận phòng nhỏ hơn ngày trả phòng, Xin mời nhập lại!");
-    else if (endDate >= startDate) {
+    if ((endDate < now) || (startDate < now)) alert("Bạn đã chọn ngày nhỏ hơn ngày hiện tại, Xin mời nhập lại!")
+    else if (endDate <= startDate) alert("Bạn đã chọn ngày nhận phòng nhỏ hơn hoặc bằng ngày trả phòng, Xin mời nhập lại!");
+    else if (endDate > startDate) {
         console.log("true");
         // Kiểm tra nhà đã được cho thuê trong 1 khoảng thời gian
         $.ajax({
@@ -20,7 +20,7 @@ function orderHome() {
             url: "http://localhost:8080/api/homeTimes/searchByHome/" + idH,
             success: function (data) {
                 console.log(data)
-                if (data.length != 0) {
+                if (data.length > 0) {
                     let flag = true;
                     for (let i = startDate.getTime(); i <= endDate.getTime(); i += oneDay) {
                         let orderDate = new Date(i);
@@ -28,25 +28,24 @@ function orderHome() {
                         for (let j = 0; j < data.length; j++) {
                             let bookingDate = (new Date(data[j].date));
                             console.log(bookingDate)
-                            if ((orderDate.getDay() === bookingDate.getDay()) && (orderDate.getMonth() === bookingDate.getMonth()) && (orderDate.getFullYear() === bookingDate.getFullYear())) {
+                            if ((orderDate.getDate() === bookingDate.getDate()) && (orderDate.getMonth() === bookingDate.getMonth()) && (orderDate.getFullYear() === bookingDate.getFullYear())) {
                                 flag = false;
                                 break;
                             }
                         }
                     }
-
                     if (!flag) {
-                        let orderStartDate = new Date(data[0].date);
-                        let orderEndDate = new Date(data[data.length - 1].date);
-                        console.log(orderStartDate);
-                        console.log(orderEndDate);
-                        alert("Nhà đang được cho thuê từ ngày " + orderStartDate.getDay() + " / " +
-                            orderStartDate.getMonth() + " / " +
-                            orderStartDate.getFullYear() + " đến ngày " +
-                            orderEndDate.getDay() + " / " +
-                            orderEndDate.getMonth() + " / " +
-                            orderEndDate.getFullYear());
-                    } else {
+                        // let orderStartDate = new Date(data[0].date);
+                        // let orderEndDate = new Date(data[data.length - 1].date);
+                        alert("Nhà đang được cho thuê !! Xin mời nhập lại ! "
+                            // + orderStartDate.getDate() + " / " +
+                            // orderStartDate.getMonth() + " / " +
+                            // orderStartDate.getFullYear() + " đến ngày " +
+                            // orderEndDate.getDate() + " / " +
+                            // orderEndDate.getMonth() + " / " +
+                            // orderEndDate.getFullYear()
+                        );
+                    }else {
                         alert("Thuê thoải mái đeee");
                         // Tạo order
                         let order = {
@@ -75,8 +74,8 @@ function orderHome() {
                             }
                         })
                     }
-                } else if (data.length == 0) {
-                    alert("Sẵn sàng cho bạn thuê !!!")
+                } else {
+                    alert("Thuê thoải mái đeee !!!")
                     // Tạo Order
                     let order = {
                         startDate: startDate,
